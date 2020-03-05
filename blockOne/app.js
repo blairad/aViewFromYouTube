6de5375424733955aa8b3039c2a5e6abd3659b5c
Andrew Blair <blair.d.andrew@gmail.com>
2020-03-05 18:03:18 +0000

    a few videos in saving for reference

---
+++ blockOne/app.js
@@ -0,0 +1,47 @@
+new Vue({
+    el:"#vue-app",
+
+    data: {
+        name: "Andrew",
+        job: ":(",
+        website: 'https://vuejs.org/',
+        websiteTag: '<a href="https://vuejs.org/">Website</a>',
+        age: 34,
+        x: 0,
+        y: 0,
+        name2: '',
+        age2: ''
+    },
+
+    methods: {
+        greet: function(time){
+            // this.name is really this.data.name. but vue lifts the data up to the instance of vue in this case new Vue
+            return "Good " + time + ' ' + this.name;
+        },
+        //the argument is related to the parameter in the html depending on button. either (1) or (10)
+        // <h2>Events</h2>
+         add: function(inc){
+            this.age += inc;
+        },
+        subtract: function(dec) {
+            this.age -= dec;
+        },
+
+        //CANVAS
+        updateXY: function(event){
+            //offsetX/Y are taken from the top left of the canvas made from the div
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        },
+        // KEYBOARD EVENTS
+        click: function(){
+            alert('you clicked me')
+        },
+        logName: function(){
+            console.log('you entered your name')
+        },
+        logAge: function(){
+            console.log('you entered your age')
+        }
+    }
+});
