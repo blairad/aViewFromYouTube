2263bcaebaf0570d83f7b871688a3f56b5b14511
Andrew Blair <blair.d.andrew@gmail.com>
2020-04-01 17:15:21 +0100

    some more examples and notes

---
+++ blockTwo/v-for/app.js
@@ -0,0 +1,19 @@
+new Vue ({
+    el: "#app",
+
+    data: {
+        name: 'Jack',
+        characters: ['mario', 'luigi', 'yoshi', 'bowser'],
+        ninjas: [
+            {name: 'Ryu', age: 29},
+            {name: 'Link', age: 22},
+            {name: 'Ken', age: 35}
+        ]
+    },
+    methods: {
+
+    },
+    computed: {
+        
+    }
+})
