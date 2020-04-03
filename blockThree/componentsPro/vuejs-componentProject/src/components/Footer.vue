4d6cd2c2ded9d4da7b84d1464f5f983cddbfd773
Andrew Blair <blair.d.andrew@gmail.com>
2020-04-03 17:24:36 +0100

    couple more examples

---
+++ blockThree/componentsPro/vuejs-componentProject/src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer>
+    <h2>{{ title }}</h2>
+  </footer>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+    
+  },
+  data () {
+    return {
+        title: 'Footer'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+footer{
+    background: #222;
+    padding: 6px;
+    
+}
+h2{
+    color: mediumvioletred;
+    text-align: center;
+}
+
+
+
+</style>
---
+++ blockThree/vueCLI/vuejs-playlist/components/Ninjas.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>List of ninjas</h1>
+    <ul>
+      <li v-for="(ninja, index) in ninjas" v-bind:key="index">{{ ninja }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ninjas: ["Bowser", "Ryu", "Ken"],
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+
+h1{
+  color:bisque
+  }
+
+</style>
